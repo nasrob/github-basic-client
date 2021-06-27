2b90076ef384f6391cda8617f8c9d0c5e5eb8a0f
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-06-27 21:12:19 +0200

    added event listeners and axios

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello Parcel !')
+import axios from 'axios'
+
+// when dom is available
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form')
+
+    form.addEventListener('submit', event => {
+        event.preventDefault() // prevent the default behavior (the page reload)
+        const username = document.querySelector('input').value
+        alert(username)
+    })
+})
